43b1586681599a9695af6a58636442d690f09d7a
Brage Ingebrigtsen <84551201+Skyppex@users.noreply.github.com>
2026-04-28 09:19:18 +0200

    chore: delete unused function

--- lua/kanji/repo.lua
+++ lua/kanji/repo.lua
@@ -63,24 +63,6 @@ function M.get_inverse_diff(path, on_done)
 	}):start()
 end
 
-function M.get_file_content(revision, path, on_done)
-	if not path or not revision then
-		return
-	end
-
-	Job:new({
-		command = "jj",
-		args = { "file", "show", "-r", revision, "--", path },
-		on_exit = function(j, exit_code, _)
-			if exit_code ~= 0 then
-				on_done(nil)
-				return
-			end
-			on_done(j:result())
-		end,
-	}):start()
-end
-
 function M.get_blame(template, path, on_done)
 	if not template or not path then
 		return
